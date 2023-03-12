926143bac87de484b03e22371ed5147bb2dafb6a
Julieta Neder <julinedercuretti1@gmail.com>
2023-03-12 12:00:46 -0300

    validation form finished

---
+++ javascript/validacion.js
@@ -0,0 +1,59 @@
+//Validación formulario de contacto
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+    input.addEventListener("blur", (input) => {
+        valida(input.target);
+    });
+});
+
+function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("formcontacto__box--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else {
+        input.parentElement.classList.add("formcontacto__box--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML =
+        mostrarMensajeDeError(tipoDeInput, input);
+    }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+];
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo nombre no puede estar vacío",
+    },
+    email: {
+        valueMissing: "El campo correo no puede estar vacío",
+        typeMismatch: "El correo no es válido",
+    },
+    asunto: {
+        valueMissing: "El campo asunto no puede estar vacío",
+    },
+    mensaje: {
+        valueMissing: "El campo mensaje no puede estar vacío",
+    },
+};
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {
+        if (input.validity[error]) {
+        console.log(tipoDeInput, error);
+        console.log(input.validity[error]);
+        console.log(mensajesDeError[tipoDeInput][error]);
+        mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    });
+    return mensaje;
+};
